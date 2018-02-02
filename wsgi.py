9c53e70f844a02d4a160b526e6562f0760d2db4c
nttalok <alok.daipuria@nttdata.com>
2018-02-02 12:51:09 -0600

    moded wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,2 +1,17 @@
-from app import app as application
+
+m flask import Flask
+from flask import render_template, url_for
+
+import os
+import socket
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return  render_template('index.html', name=os.getenv("NAME", "world"), hostname=socket.gethostname())
+
+if __name__ == "__main__":
+        app.run()
 
