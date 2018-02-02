eebd8bfa388f53e2efc17c5728d49ad0a9a02039
nttalok <alok.daipuria@nttdata.com>
2018-02-02 11:27:22 -0600

    added ports

--- app.py
+++ app.py
@@ -12,3 +12,5 @@ app = Flask(__name__)
 def hello():
 	return  render_template('index.html', name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
+if __name__ == "__main__":
+	app.run(port=8080)
